f362f2046024d6c9c0adcd7bbc1d3ae3312a8985
danny chavez <dannychavez141@gmail.com>
2021-06-23 09:58:24 -0500

    limpieza 3

--- control/conexion.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-
-$mysqli = new mysqli('localhost','root','','comercio');  
-  //$this->bd = new mysqli("179.61.12.105","premiumc_premium","Losloles54","premiumc_premiumcollege");
-	if($mysqli->connect_errno){
-        echo 'Conexion Fallida : ', $this->bd->connect_errno;}
--- control/repararinicial.php
+++
@@ -1,5 +0,0 @@
-<?php
-include './cConexion.php';
-include '../modelo/dboMatricula.php';
-$dbomatricula=new dboMatricula();
-$dbomatricula->crearnotas(3);
